175f65434b1f8b7e2a2531849db0c2c7919bd695
Brendan Manning <>
2021-03-27 18:27:48 -0400

    Some of the api callbacks setup

--- app/src/main/java/edu/temple/bookshelf/BookList.java
+++ app/src/main/java/edu/temple/bookshelf/BookList.java
@@ -1,6 +1,12 @@
 package edu.temple.bookshelf;
 
+import com.android.volley.RequestQueue;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.function.Function;
 
 public class BookList {
 
@@ -11,6 +17,29 @@ public class BookList {
         for(Book b : books) {
             list.add(b);
         }
+    }
+
+    public static void fromSearch(RequestQueue requestQueue, String search, Function<BookList, Void> callback) {
+        System.out.println("In BookList.fromSearch");
+        Api.search(requestQueue, search, jsonObject -> {
+            System.out.println("In Api.search callback");
+            try {
+                Book[] books = new Book[jsonObject.length()];
+
+                for (int i = 0; i < jsonObject.length(); i++) {
+                    JSONObject bookJson = jsonObject.getJSONObject(String.valueOf(i));
+                    books[i] = new Book(bookJson.getString("title"), bookJson.getString("author"));
+                }
+
+                BookList bookList = new BookList(books);
+                callback.apply(bookList);
+
+            } catch (JSONException e) {
+                System.out.println("There was a JSONException");
+            }
+            return null;
+        });
+        callback.apply(new BookList());
     }
 
     public ArrayList<Book> toArrayList() {
--- app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
+++ app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
@@ -7,6 +7,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import java.util.function.Function;
+
 public class BookSearchActivity extends AppCompatActivity {
 
     EditText searchEditText;
@@ -14,8 +19,11 @@ public class BookSearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_search);
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
 
         /**
          * BIND UI COMPONENTS
@@ -27,7 +35,12 @@ public class BookSearchActivity extends AppCompatActivity {
          * BIND UI HANDLERS
          */
         searchButton.setOnClickListener(v -> {
-
+            BookList.fromSearch(requestQueue, searchEditText.getText().toString(), bookList -> {
+                System.out.println("Got a booklist");
+                System.out.println(bookList);
+                return null;
+            });
+            requestQueue.start();
         });
 
     }
