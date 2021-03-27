d561128a233718f304cd38a77d819a9c863440a1
Brendan Manning <>
2021-03-27 17:35:49 -0400

    Bind UI items in the search activity

--- app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
+++ app/src/main/java/edu/temple/bookshelf/BookSearchActivity.java
@@ -3,12 +3,32 @@ package edu.temple.bookshelf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BookSearchActivity extends AppCompatActivity {
+
+    EditText searchEditText;
+    Button searchButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_search);
+
+        /**
+         * BIND UI COMPONENTS
+         */
+        searchEditText = findViewById(R.id.searchSearchInput);
+        searchButton = findViewById(R.id.searchSearchButton);
+
+        /**
+         * BIND UI HANDLERS
+         */
+        searchButton.setOnClickListener(v -> {
+
+        });
+
     }
 }
